9367905391d7825bf2c177d336912a4a639f841a
jmcsuite <73972102+jmcsuite@users.noreply.github.com>
2022-01-16 17:31:35 -0600

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+color = (143, 136, 63)
+
+# Add display
+def display(im, box, it):
+    nbox = box[it]
+    for i in range(len(nbox)):
+        cv2.line(im, (int(nbox[i][0]), int(nbox[i][1])), (int(nbox[(i + 1) % len(nbox)][0]), int(nbox[(i + 1) % len(nbox)][1])), color, 6)
+
+# Receives binary input  
+def processInput(req, res):
+    r""""
+    Receives binary input (a path of the image to analyze)
+    Returns if any qr was found
+    Retur am array of the qr data
+    Returns path of the analyzed image
+    """
+    input = decodeInput(req)
+    possible ,results, path = getQRs(input)
+    res['possible'] = possible
+    res['results'] = results
+    res['path'] = path
+
+def getQRs(name):
+    inputImage = cv2.imread('{}'.format(name))
+    decoder = cv2.QRCodeDetector()
+    data, data1, data2, data3 = decoder.detectAndDecodeMulti(inputImage)
+    if(data):
+        for i in range(len(data2)):
+            display(inputImage, data2, i)
+        cv2.imwrite('analizado/{}'.format(name), inputImage)
+        return data, data1, 'analizado/{}'.format(name)
+    else:
+        return data, data1, name
+
+
+
+
+def decodeInput(input):
+    r""""
+    Receives encoded utf-8.
+    it should be a single string, the relative path of the image to process
+    """ 
+    input = input.decode("utf-8")
+    return input
+
---
+++ server.py
@@ -0,0 +1,30 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+from logic import processInput
+
+HOST = '127.0.0.1'
+PORT = 8080
+
+class JuanHTTP(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        content_size = int(self.headers['Content-Length'])
+        #print(content_size)
+
+        input = self.rfile.read(content_size)
+        res = dict()
+        ans = processInput(input, res)
+        #a = a.decode("utf-8")
+        #a = a.split("\n")
+        #print(a)
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        
+        self.wfile.write(bytes(json.dumps(res), "utf-8"))
+    
+server = HTTPServer((HOST, PORT), JuanHTTP)
+print("Runnin server in port: {}".format(PORT))
+
+server.serve_forever()
+
