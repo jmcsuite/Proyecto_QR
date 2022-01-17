7997c120a6f8ca8ae53e2244339d60092d8f19b3
jmcsuite <jmcsuite@hotmail.com>
2022-01-16 22:08:43 -0600

    a

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,8 +1,3 @@
-import './FrontEnd/estilos.css';
-const {loadImg} = require('canvas');
-var express = require('express');
-var app = express();
-app.get('/', function(req, res){
-  res.render('layout', {title: 'frontpage'});
-});
-module.exports = app;
+function prueba(){
+  console.log(3333)
+}
--- app.js
+++ app.js
@@ -2,25 +2,32 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
-app.use('/', express.static(__dirname + '/FrontEnd'));
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
+app.use('/static', express.static(__dirname + '/FrontEnd'));
 
+//const bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: true, limit: '50mb'}));
+
+//app.use(express.json({limit: '50mb'}))
 
 app.get('/', (req, res) => {
   //var pinky = require('../FrontEnd/index.html');
   console.log("Servidor prueba en el ge  dasdasdt");
-  res.send("Hello world!");
+  //res.send("Hello world!");
   //res.send(pinky);
-  //res.sendFile(__dirname + '/FrontEnd/index.html');
+  res.sendFile(__dirname + '/FrontEnd/index.html');
 })
 
-app.post('/', (req, res) => {
+app.post('/', upload.single('imagen'), (req, res) => {
   //var pinky = require('../FrontEnd/index.html');
   console.log("Servidor prueba en el post");
   //res.send("Hello world!");
   //res.send(pinky);
-  console.log(req.body)
-  res.send('posttt')
+  console.log(req.file)
+  let a = "poooss"
+  res.json(a)
 })
 
 
